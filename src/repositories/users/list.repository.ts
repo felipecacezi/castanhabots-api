020ef7678e2a0db9efb6efc21255ba7303fabac8
Felipe Ccezi <felipecacezi@gmail.com>
2025-06-11 21:42:28 -0300

    feat: implementação da rota list

---
+++ src/repositories/users/list.repository.ts
@@ -0,0 +1,15 @@
+import { User as UserEntity } from '../../entities/User.entity'
+import { AppDataSource } from '../../config/data-source'
+
+export const listAll = async () => {
+    return await AppDataSource.getRepository(UserEntity)
+        .createQueryBuilder("user")
+        .getMany();        
+}
+
+export const listByEmail = async (email: string) => {
+    return await AppDataSource.getRepository(UserEntity)
+        .createQueryBuilder("user")
+        .where(`user.email like :email`, { email })
+        .getMany();        
+}
